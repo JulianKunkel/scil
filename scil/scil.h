961de05cb0cd16ba2ccf75ef57e1eb3de1ee5906
Julian Kunkel <juliankunkel@googlemail.com>
2015-10-26 14:19:13 +0100

    Header Beispiel

---
+++ scil/scil.h
@@ -0,0 +1,25 @@
+#ifndef SCIL_HEADER_
+#define SCIL_HEADER_
+
+typedef struct{
+  // relative tolerance e.g. 1 means the value 2 can become 1-3
+  double relative_tolerance;
+  // 1 means 1% tolerance
+  double absolute_tolerance_percent; 
+} scil_hints;
+
+
+// TODO later: hide the structure in an internal header
+typedef struct{
+  scil_hints hints;
+  //...
+} scil_context;
+
+
+int scil_create_compression_context(scil_context * out_ctx, scil_hints * hints);
+
+int scil_compress(scil_context * ctx, size_t count, float/double * data, char * compressed_buf, size_t * out_size);
+
+int scil_decompress(scil_context * ctx, char * compressed_buf, float/double * data_out, size_t * out_size);
+
+#endif
