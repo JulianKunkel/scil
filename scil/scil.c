a3eb60b33e5bbe05d67a94e6c49312b19898bc12
Abyss333 <armin-scha@hotmail.de>
2015-11-02 22:50:30 +0100

    Changed error to absolute_tolerance

--- scil/scil.c
+++ scil/scil.c
@@ -1,20 +1,59 @@
 #include "scil.h"
 
+#include <stdint.h>
 #include <assert.h>
+#include <math.h>
 
-int scil_create_compression_context(scil_context * out_ctx, scil_hints * hints){
+static double find_min(const double* buf, const size_t size){
 
-	out_ctx->hints = hints;
+	assert(buf != NULL);
+
+	double result = INFINITY;
+	for(size_t i = 0; i < size; ++i)
+	{
+		if(buf[i] < result)
+			result = buf[i];
+	}
+	return result;
+}
+
+static double find_max(const double* buf, const size_t size){
+
+	assert(buf != NULL);
+
+	double result = -INFINITY;
+	for(size_t i = 0; i < size; ++i)
+	{
+		if(buf[i] > result)
+			result = buf[i];
+	}
+	return result;
+}
+
+static int find_min_max(double* min, double* max, const double* buf, const size_t size){
+
+	assert(buf != NULL);
+
+	*min = INFINITY;
+	*max = -INFINITY;
+
+	for(size_t i = 0; i < size; ++i)
+	{
+		if(buf[i] < *min)
+			*min = buf[i];
+		if(buf[i] > *max)
+			*max = buf[i];
+	}
 
 	return 0;
 }
 
-static uint8_t get_needed_bit_count(const double min_value, const double max_value, const double error_step){
+static uint8_t get_needed_bit_count(const double min_value, const double max_value, const double absolute_tolerance){
 
 	assert(max_value > min_value);
 	assert(error_step > 0);
 
-	return (uint8_t)ceil(log2(1 + (max_value - min_value) / error_step)); //Potentieller Fehler, wahrscheinlich nicht
+	return (uint8_t)ceil(log2(1 + (max_value - min_value) / (2 * absolute_tolerance))); //TODO: Test this
 }
 
 static size_t round_up_byte(const size_t bits){
@@ -25,24 +64,44 @@ static size_t round_up_byte(const size_t bits){
 	return 1 + (bits - a) / 8;
 }
 
-static uint64_t int_repres(const double num, const double min, const double err){
+static uint64_t int_repres(const double num, const double min, const double absolute_tolerance){
 
 	assert(num >= min);
 
-	return (uint64_t)round((num - min) / err);
+	return (uint64_t)round((num - min) / (2 * absolute_tolerance)); //TODO: Test this
 }
 
-static double double_repres(const uint64_t num, const double min, const double err){
+static double double_repres(const uint64_t num, const double min, const double absolute_tolerance){
 
-	return min + num * err;
+	return min + num * 2 * absolute_tolerance; //TODO: Test this
 }
 
-int scil_compress(scil_context * ctx, size_t count, float/double * data, char * compressed_buf, size_t * out_size){
+int scil_create_compression_context(scil_context * out_ctx, scil_hints * hints){
+
+	out_ctx->hints = hints;
 
 	return 0;
 }
 
-int scil_decompress(scil_context * ctx, char * compressed_buf, float/double * data_out, size_t * out_size){
+int scil_compress(scil_context* ctx, char* compressed_buf_out, size_t* out_size, const double* data_in, const size_t in_size);
+
+	assert(ctx != NULL);
+	assert(data_in != NULL);
+
+	//Finding minimum and maximum values in data
+	double min, max;
+	find_min_max(&min, &max, data_in, in_size);
+
+	//Get needed bits per number in data
+	uint8_t bits = get_needed_bit_count(min, max, ctx->hints->absolute_tolerance);
+
+	return 0;
+}
+
+int scil_decompress(scil_context* ctx, double* data_out, size_t* out_size, const char* compressed_buf_in, const size_t in_size);
+	
+	assert(ctx != NULL);
+	assert(compressed_buf_in != NULL);
 
 	return 0;
 }
