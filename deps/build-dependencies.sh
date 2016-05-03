1f2917ea6d9f8d84299afcceb39a6bb657f504a4
Julian Kunkel <juliankunkel@googlemail.com>
2016-05-03 10:56:17 +0200

    Build (new) dependency of OpenJPEG.

--- deps/build-dependencies.sh
+++ deps/build-dependencies.sh
@@ -4,16 +4,30 @@ echo "Building dependencies for SCIL from third-party software"
 
 ZFP=zfp-0.5.0
 FPZIP=fpzip-1.1.0
+OPENJPG=2.1
 
 if [[ ! -e ${FPZIP}.tar.gz ]] ; then
 	wget http://computation.llnl.gov/projects/floating-point-compression/download/$FPZIP.tar.gz
 	wget http://computation.llnl.gov/projects/floating-point-compression/download/$ZFP.tar.gz
+	wget https://github.com/uclouvain/openjpeg/archive/version.$OPENJPG.tar.gz
 	for x in *.gz ; do
 		tar -xf $x
 	done
 fi
 
 BUILD=0
+
+JPEG_INSTALL=$PWD/openjpeg-version.$OPENJPG/install
+
+if [[ ! -e $JPEG_INSTALL ]] ; then
+	echo "  Building openjpg"
+	pushd openjpeg-version.$OPENJPG
+	mkdir build
+	cd build
+	cmake ../ -DCMAKE_INSTALL_PREFIX=$JPEG_INSTALL -DCMAKE_C_FLAGS="-Wl,--rpath=$JPEG_INSTALL/lib/"
+	make install
+	popd
+fi
 
 if [[ ! -e libzfp.a ]] ; then
 	echo "  Building fpzip shared library"
