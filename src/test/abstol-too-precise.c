f18e35798875914ade0865eb09af6547f0b04623
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-05-17 18:11:07 +0200

    Added test for too strict precision parameters in abstol algorithm

---
+++ src/test/abstol-too-precise.c
@@ -0,0 +1,13 @@
+//#include <stdint.h>
+
+#include <scil-util.h>
+#include <scil.h>
+
+#include <basic-patterns.h>
+
+int main(int argc, char** argv)
+{
+    const size_t count = 100;
+
+    double* buffer = (double*)SAFE_MALLOC(count * sizeof(double));
+}
