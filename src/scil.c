f4bbff31a136adbe754de8522df47e75579ec5a8
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-05-23 12:09:40 +0200

    Fixed remaining merge cconflict

--- src/scil.c
+++ src/scil.c
@@ -245,23 +245,6 @@ void scil_init_dims_2d(scil_dims* dims, size_t dim1, size_t dim2)
     dims->length[0] = dim1;
     dims->length[1] = dim2;
 }
-<<<<<<< HEAD
-void scil_init_dims_3d(scil_dims* dims, size_t dim1, size_t dim2, size_t dim3)
-{
-    dims->dims      = 3;
-    dims->length[0] = dim1;
-    dims->length[1] = dim2;
-    dims->length[2] = dim3;
-}
-void scil_init_dims_4d(
-    scil_dims* dims, size_t dim1, size_t dim2, size_t dim3, size_t dim4)
-{
-    dims->dims      = 4;
-    dims->length[0] = dim1;
-    dims->length[1] = dim2;
-    dims->length[2] = dim3;
-    dims->length[2] = dim4;
-=======
 
 void scil_init_dims_3d(scil_dims* dims, size_t dim1, size_t dim2, size_t dim3){
 	dims->dims = 3;
@@ -281,7 +264,6 @@ void scil_init_dims_4d(scil_dims* dims, size_t dim1, size_t dim2, size_t dim3, s
 void scil_copy_dims_array(scil_dims* out_dims, scil_dims in_dims){
 	out_dims->dims = in_dims.dims;
 	memcpy(out_dims->length, & in_dims.length, sizeof(size_t)* in_dims.dims);
->>>>>>> master
 }
 
 void scil_init_dims_array(scil_dims* dims, uint8_t count, const size_t* length)
