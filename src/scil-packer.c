2d2a06f37ece5c4f568134abd4bb9f46f8956922
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-06-17 19:25:41 +0200

    Wrote interface for encapsulated scil-packer

---
+++ src/scil-packer.c
@@ -0,0 +1,19 @@
+#include <scil-packer.h>
+
+int scil_pack(byte* const restrict buf_out,
+              const uint64_t* const restrict buf_in,
+              const size_t count,
+              const uint8_t bits_per_value){
+
+
+    return 0;
+}
+
+int scil_unpack(uint64_t* const restrict buf_out,
+                const byte* const restrict buf_in,
+                const size_t count,
+                const uint8_t bits_per_value){
+
+
+    return 0;
+}
