592ed62a7d85e6fbc9fe9e5324a8b965caedffe0
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-06-18 14:20:20 +0200

    Nothing important

--- src/scil-packer.c
+++ src/scil-packer.c
@@ -3,8 +3,25 @@
 int scil_pack(byte* const restrict buf_out,
               const uint64_t* const restrict buf_in,
               const size_t count,
-              const uint8_t bits_per_value){
+              const uint8_t bits_per_value)
+{
+    size_t bit_index = 0;
 
+    for(size_t i = 0; i < count; ++i){
+        size_t start_byte = bit_index / 8;
+        size_t end_byte   = (bit_index + bits_per_value) / 8;
+        uint8_t bit_offset = bit_index % 8;
+
+        if(start_byte == end_byte){
+            buf_out[start_byte] |= buf_in[i] << (8 - bit_offset - bits_per_value);
+        } else if(start_byte + 1 == end_byte){
+            buf_out[start_byte] |= buf_in[i] << (8 - bit_offset - bits_per_value);
+        } else {
+
+        }
+
+        bit_index += bits_per_value;
+    }
 
     return 0;
 }
