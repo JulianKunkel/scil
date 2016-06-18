b31f8d39c63c21d565fd0fa2e68403bb6ddfc44e
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-06-18 14:21:12 +0200

    Merged master

--- src/scil-util.h
+++ src/scil-util.h
@@ -247,5 +247,25 @@ void scilU_print_dims(scil_dims dims);
 
 void scilU_plot(const char* name, scil_dims dims, double * buffer_in);
 
+// like memcopy but swaps the order
+#define scilU_reverse_copy(buffer, src, size) do { char * _o = (char*) buffer; char * _s = ((char *) src) + size - 1; for(int _c=size; _c > 0; _c-- ) { *_o = *_s ; _s--; _o++; } } while(0)
+
+#ifdef SCIL_LITTLE_ENDIAN
+
+#define scilU_pack4(buffer, val) *((int32_t*)buffer) = val
+#define scilU_unpack4(buffer, result_p) *result_p = *((int32_t*) buffer)
+
+#define scilU_pack8(buffer, val) *((int64_t*)buffer) = val
+#define scilU_unpack8(buffer, result_p) *result_p = *((int64_t*) buffer)
+
+#else
+
+#define scilU_pack4(buffer, val) scilU_reverse_copy(buffer, &val, 4)
+#define scilU_unpack4(buffer, result_p) scilU_reverse_copy(result_p, buffer, 4)
+
+#define scilU_pack8(buffer, val)  scilU_reverse_copy(buffer, &val, 8)
+#define scilU_unpack8(buffer, result_p) scilU_reverse_copy(result_p, buffer, 8)
+
+#endif
 
 #endif /* UTIL_H */
