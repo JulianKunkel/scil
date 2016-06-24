1c29bb4ca764226b05d9e948c6a6fcf3fa7200e7
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-06-24 18:25:55 +0200

    Added test for scil-swager

---
+++ src/test/swage.c
@@ -0,0 +1,43 @@
+#include <scil-swager.h>
+
+#include <scil-util.h>
+
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(void){
+
+    const uint32_t count = 100;
+    const uint8_t max_bits_per_value = 63;
+
+    const uint64_t buf_size = count * sizeof(uint64_t);
+
+    uint64_t* buf_in  = (uint64_t*)SAFE_MALLOC(buf_size);
+    byte*     buf_out = (byte*)SAFE_MALLOC(buf_size);
+    uint64_t* buf_end = (uint64_t*)SAFE_MALLOC(buf_size);
+
+    srand((unsigned)time(NULL));
+
+    for(uint8_t i = 1; i < max_bits_per_value; ++i)
+    {
+        for(uint64_t i = 0; i < count; ++i)
+        {
+            buf_in[i] = (uint64_t)floor( ((double)rand() / RAND_MAX) * (1 << i) );
+        }
+
+        scil_swage(buf_out, buf_in, count, bits_per_value);
+        scil_unswage(buf_end, buf_out, count, bits_per_value);
+
+        for(uint64_t i = 0; i < count; ++i)
+        {
+            assert(buf_in[i] == buf_end[i]);
+        }
+    }
+
+    free(buf_in);
+    free(buf_out);
+    free(buf_end);
+
+    return 0;
+}
