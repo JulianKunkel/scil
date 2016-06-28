c67195cf50b8f29f3aaf1c23309f68716eec0362
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-06-28 21:50:13 +0200

    Fixed compiler errors

--- src/test/quantize.c
+++ src/test/quantize.c
@@ -22,16 +22,16 @@ int main(void){
             buf_in[j] = -1000000.0 + ((double)rand() / (double)RAND_MAX) * 2000000.0;
         }
 
+        double absolute_tolerance = pow(2.0, -i);
+
         double minimum, maximum;
         scil_find_minimum_maximum_double(buf_in, count, &minimum, &maximum);
 
-        printf("#Cycle %d with following attributes:\n");
+        printf("#Cycle %d with following attributes:\n", i);
         printf("#Absolute tolerance: %f, Minimum: %f, Maximum: %f\n", absolute_tolerance, minimum, maximum);
 
-        double absolute_tolerance = pow(2.0, -i);
-
-        uint64_t bits_needed = scil_calculate_bits_needed(minimum, maximum, absolute_tolerance);
-        uint8_t ret = scil_quantize_buffer_minmax_double(buf_out, buf_in, count, absolute_tolerance);
+        uint64_t bits_needed = scil_calculate_bits_needed_double(minimum, maximum, absolute_tolerance);
+        uint8_t ret = scil_quantize_buffer_minmax_double(buf_out, buf_in, count, absolute_tolerance, minimum, maximum);
 
         if(bits_needed > 64){
             // Failure testing
@@ -53,7 +53,7 @@ int main(void){
             printf("#Values before and after a quant-unquant-cycle.\n");
             printf("#before,after\n");
             for(uint16_t j = 0; j < count; ++j){
-                printf("%f,%f\n", buf_in[j], buf_end[j])
+                printf("%f,%f\n", buf_in[j], buf_end[j]);
                 if( fabs(buf_in[j] - buf_end[j]) > absolute_tolerance ){
                     printf("#FAILURE: value at index %d was after quantizing not in the tolerated error margin.\n", j);
                     return 1;
