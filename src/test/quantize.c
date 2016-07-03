b338bc4dc7ccee9c07eb50100acad143fd0ea0d7
Armin Schaare <3schaare@informatik.uni-hamburg.de>
2016-07-04 00:57:04 +0200

    Changed IO

--- src/test/quantize.c
+++ src/test/quantize.c
@@ -7,7 +7,7 @@
 
 int main(void){
 
-    const size_t count = 10;
+    const size_t count = 1000000;
 
     double*   buf_in  = (double*)SAFE_MALLOC(count * sizeof(double));
     uint64_t* buf_out = (uint64_t*)SAFE_MALLOC(count * sizeof(uint64_t));
@@ -16,10 +16,10 @@ int main(void){
     srand((unsigned)time(NULL));
 
     printf("#Testing scil-quantizer\n\n");
-    for(uint16_t i = 1; i < 100; ++i){
+    for(uint16_t i = 1; i < 64; ++i){
 
-        for(uint16_t j = 0; j < count; ++j){
-            buf_in[j] = -1.0 + ((double)rand() / (double)RAND_MAX) * 2.0;
+        for(size_t j = 0; j < count; ++j){
+            buf_in[j] = 1000.0 * ( -1.0 + ((double)rand() / (double)RAND_MAX) * 2.0 );
         }
 
         double absolute_tolerance = pow(2.0, -i);
@@ -34,18 +34,25 @@ int main(void){
 
         printf("#Bits needed per value: %lu\n", bits_needed);
 
+        /*
         datatype_cast_double max_union, max_union_m1;
         max_union.f = maximum;
         max_union_m1.f = maximum;
         max_union_m1.p.mantissa -= 1;
 
         double max_prec = max_union.f - max_union_m1.f;
+        */
 
-        printf("Maximum precision: %.20f\n", max_prec);
+        double d_max_prec = maximum * pow(10.0, -15.0);
+        printf("#Maximum guaranteed double precision: %.20f\n", d_max_prec);
 
-        if( max_union.f - max_union_m1.f > absolute_tolerance / 2){
-            printf("#!!!Risk of unsufficient double precision!!!\n");
+        if(d_max_prec > absolute_tolerance){
+            printf("#!!!Risk of insufficient double precision!!!\n");
+            printf("#Error tol.: %.20f\n", absolute_tolerance );
+            printf("#Max. prec.: %.20f\n", d_max_prec);
         }
+
+        printf("#Checking values before and after a quant-unquant-cycle.\n");
 
         uint8_t ret = scil_quantize_buffer_minmax_double(buf_out, buf_in, count, absolute_tolerance, minimum, maximum);
 
@@ -61,20 +68,20 @@ int main(void){
         }
         else{
             // Success testing
-
-            printf("#Needed bits for quantized values <= 64.\n\n");
+            printf("#Needed bits for quantized values <= 64.\n");
 
             scil_unquantize_buffer_double(buf_end, buf_out, count, absolute_tolerance, minimum);
 
-            printf("#Values before and after a quant-unquant-cycle.\n");
-            printf("#before,after\n");
-            for(uint16_t j = 0; j < count; ++j){
-                printf("%+.20f,%+.20f\n", buf_in[j], buf_end[j]);
+            //printf("#before,after\n");
+            for(size_t j = 0; j < count; ++j){
+                //printf("%+.20f,%+.20f\n", buf_in[j], buf_end[j]);
                 if( fabs(buf_in[j] - buf_end[j]) > absolute_tolerance ){
-                    printf("#FAILURE: value at index %d was after quantizing not in the tolerated error margin.\n", j);
+                    printf("#FAILURE: value at index %lu was after quantizing not in the tolerated error margin.\n", j);
+                    printf("#Before: %+.20f\n", buf_in[j]);
+                    printf("#After:  %+.20f\n", buf_end[j]);
                     printf("#Difference: %.20f\n", fabs(buf_in[j] - buf_end[j]) );
                     printf("#Error tol.: %.20f\n", absolute_tolerance );
-                    printf("#Max. prec.: %.20f\n", max_prec);
+                    printf("#Max. prec.: %.20f\n", d_max_prec);
                     return 1;
                 }
             }
@@ -82,7 +89,7 @@ int main(void){
         }
     }
 
-    printf("#SUCCESS");
+    printf("#SUCCESS\n");
 
     return 0;
 }
