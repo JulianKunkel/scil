4f9af46090210f4161b94a81c4ae2ef4f95f7ecb
Julian M. Kunkel <juliankunkel@googlemail.com>
2016-07-22 09:58:18 +0200

    Tests added.

---
+++ src/test/abstol-boundary-error.c
@@ -0,0 +1,42 @@
+#include <scil-util.h>
+#include <scil.h>
+
+#include <scil-internal.h>
+#include <scil-patterns.h>
+
+int main(void)
+{
+    const size_t count = 10;
+
+    double source[] = {0, 1, 2, 3,4,5,6,7,8,9};
+
+    scil_dims dims;
+    scil_init_dims_1d(&dims, count);
+
+    size_t dest_size = scil_compress_buffer_size_bound(SCIL_TYPE_FLOAT, &dims);
+    byte* dest       = (byte*) SAFE_MALLOC(dest_size);
+
+    scil_hints hints;
+    scil_init_hints(&hints);
+    hints.force_compression_methods = "abstol";
+    hints.absolute_tolerance        = 1;
+
+    scil_context_p ctx;
+    size_t compressed_size;
+    scil_create_compression_context(&ctx, SCIL_TYPE_DOUBLE, 0, NULL, &hints);
+
+    int ret = scil_compress(dest, dest_size, source, &dims, &compressed_size, ctx);
+    // scil_decompress();
+
+    scil_hints out_accuracy;
+    ret = scil_validate_compression(SCIL_TYPE_FLOAT, source, & dims, dest, compressed_size,  ctx, & out_accuracy);
+
+    scil_hints_print(& out_accuracy);
+
+    assert( ret == 0);
+
+    free(dest);
+    free(ctx);
+
+    return ret;
+}
