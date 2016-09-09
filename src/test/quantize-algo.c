5dcb64d4b9a354827afac55cbf94a8a82d48eccf
Armin Schaare <armin-scha@hotmail.de>
2016-09-09 02:43:07 +0200

    Enhanced quantize test to include float datatype

--- src/test/quantize-algo.c
+++ src/test/quantize-algo.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 
 #include <scil.h>
 
-int main(void){
+size_t count = 1000;
+scil_hints hints;
+scil_dims dims;
+scil_context_p context;
 
-    size_t count = 10000;
+int test_quantize_float(){
 
-    scil_hints hints;
-    scil_init_hints(&hints);
-    hints.absolute_tolerance = 0.005;
-    hints.force_compression_methods = "9";
+    scil_create_compression_context(&context, SCIL_TYPE_FLOAT, 0, NULL, &hints);
 
-    scil_context_p context;
+    size_t uncompressed_size = scil_get_data_size(SCIL_TYPE_FLOAT, &dims);
+    size_t compressed_size   = scil_compress_buffer_size_bound(SCIL_TYPE_FLOAT, &dims);
+
+    float* buffer_in  = (float*)malloc(uncompressed_size);
+    byte* buffer_out  = (byte*)malloc(compressed_size);
+    byte* buffer_tmp  = (byte*)malloc(compressed_size / 2);
+    float* buffer_end = (float*)malloc(uncompressed_size);
+
+    for(size_t i = 0; i < count; ++i){
+        buffer_in[i] = ((float)rand()/RAND_MAX - 0.5f) * 200;
+    }
+
+    size_t out_size;
+    scil_compress(buffer_out, compressed_size, buffer_in, &dims, &out_size, context);
+    scil_decompress(SCIL_TYPE_FLOAT, buffer_end, &dims, buffer_out, out_size, buffer_tmp);
+
+    printf("#Testing for float datatype.\n");
+    printf("#Index,Value,Value after comp-decomp,Abs-difference\n");
+    for (size_t i = 0; i < count; ++i) {
+        float absdiff = fabsf(buffer_end[i] - buffer_in[i]);
+        printf("%lu,%f,%f,%f\n", i, buffer_in[i], buffer_end[i], absdiff);
+        if ((double)absdiff > hints.absolute_tolerance) {
+            printf("#FAILURE: Absolute difference bigger than tolerance.\n");
+            return 1;
+        }
+    }
+
+    printf("#Buffer size,Buffer size after compression\n");
+    printf("%lu,%lu\n", uncompressed_size, out_size);
+
+    free(buffer_in);
+    free(buffer_out);
+    free(buffer_tmp);
+    free(buffer_end);
+
+    return 0;
+}
+int test_quantize_double(){
+
     scil_create_compression_context(&context, SCIL_TYPE_DOUBLE, 0, NULL, &hints);
-
-    scil_dims dims;
-    scil_init_dims_1d(&dims, count);
 
     size_t uncompressed_size = scil_get_data_size(SCIL_TYPE_DOUBLE, &dims);
     size_t compressed_size   = scil_compress_buffer_size_bound(SCIL_TYPE_DOUBLE, &dims);
@@ -33,9 +70,15 @@ int main(void){
     scil_compress(buffer_out, compressed_size, buffer_in, &dims, &out_size, context);
     scil_decompress(SCIL_TYPE_DOUBLE, buffer_end, &dims, buffer_out, out_size, buffer_tmp);
 
-    printf("#Value,Value after comp-decomp,Difference\n");
+    printf("#Testing for double datatype.\n");
+    printf("#Index,Value,Value after comp-decomp,Abs-difference\n");
     for (size_t i = 0; i < count; ++i) {
-        printf("%f,%f,%f\n", buffer_in[i], buffer_end[i], buffer_end[i] - buffer_in[i]);
+        double absdiff = fabs(buffer_end[i] - buffer_in[i]);
+        printf("%lu,%f,%f,%f\n", i, buffer_in[i], buffer_end[i], absdiff);
+        if (absdiff > hints.absolute_tolerance) {
+            printf("#FAILURE: Absolute difference bigger than tolerance.\n");
+            return 2;
+        }
     }
 
     printf("#Buffer size,Buffer size after compression\n");
@@ -46,7 +89,29 @@ int main(void){
     free(buffer_tmp);
     free(buffer_end);
 
-    //scil_destroy_compression_context(&context);
-
     return 0;
+}
+
+int main(void){
+
+    srand((unsigned)time(NULL));
+
+    scil_init_hints(&hints);
+    hints.absolute_tolerance = 0.05;
+    hints.force_compression_methods = "9";
+
+    scil_init_dims_1d(&dims, count);
+
+    unsigned char ret = 0;
+
+    ret += test_quantize_float();
+    ret += test_quantize_double();
+
+    if (ret == 0) { printf("#No Errors\n");}
+    else {
+        if (ret & 1) { printf("#Error in testing float\n");}
+        if (ret & 2) { printf("#Error in testing double\n");}
+    }
+
+    return ret;
 }
