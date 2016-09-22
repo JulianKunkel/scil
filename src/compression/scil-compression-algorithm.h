bac3149ef350dcb5efc0e4dff86b683a47d531d0
Armin Schaare <armin-scha@hotmail.de>
2016-09-22 23:01:34 +0200

    Moved scil-compression-algorithm to compression folder

---
+++ src/compression/scil-compression-algorithm.h
@@ -0,0 +1,8 @@
+#ifndef SCIL_COMPRESSION_ALGORITHM_H
+#define SCIL_COMPRESSION_ALGORITHM_H
+
+#include <scil-cca.h>
+
+scil_compression_algorithm_t * scilI_find_compressor_by_name(const char * name);
+
+#endif // SCIL_COMPRESSION_ALGORITHM_H
