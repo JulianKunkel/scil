44a925bd1011fd327bd36f2aae82cdd3f619518e
Julian M. Kunkel <juliankunkel@googlemail.com>
2016-12-28 18:57:55 +0100

    Support multiple sections with different datatypes.

--- dev/create-datatype-variants.py
+++ dev/create-datatype-variants.py
@@ -10,18 +10,8 @@ import fnmatch
 def replaceFunc(data, d):
   return data.replace("<DATATYPE>", d).replace("<DATATYPE_UPPER>", d.upper().replace("_T", ""))
 
-def parsefile(infile, outfile):
-  DATATYPES=["float","double"]
+def createFunctionList(datatypes_list):
   DATATYPES_FULL=["float","double","int8_t","int16_t","int32_t","int64_t"]
-
-  fin = open(infile, "r")
-  data = fin.read()
-  fin.close()
-  m = re.search("//Supported datatypes:(.*)", data)
-  if (m):
-    datatypes_list = m.group(1).split(" ")
-  else:
-    datatypes_list = DATATYPES
 
   datatypes_functions = [ "NULL" for x in DATATYPES_FULL for y in ["compress", "decompress"]]
   datatypes_supported = []
@@ -32,34 +22,53 @@ def parsefile(infile, outfile):
       datatypes_supported.append(d)
       datatypes_functions[pos*2] = "compress_" + d
       datatypes_functions[pos*2+1] = "decompress_" + d
+  return datatypes_functions
 
-  m = re.search("CREATE_INITIALIZER[(](.*)[)]", data)
-  if(m):
-    init_name = m.group(1)
-    datatypes_list = []
-    for d in datatypes_functions:
-      if (d != "NULL"):
-        datatypes_list.append(init_name + "_" + d)
-      else:
-        datatypes_list.append(d)
-    datatype_list = ",\n      ".join( datatypes_list )
-    data = re.sub("CREATE_INITIALIZER(.*)", datatype_list, data)
+
+def parsefile(infile, outfile):
+  datatypes_list = ["float","double"]
+  fin = open(infile, "r")
+  data = fin.read()
+  fin.close()
 
   # identify the regions to replace
   lines = data.split("\n")
   outData = []
   repeatData = []
   repeat = False
+  all_supported = []
+
   for l in lines:
+    m = re.search("//Supported datatypes:(.*)", l)
+    if (m):
+      datatypes_list = m.group(1).strip().split(" ")
+      all_supported.extend(datatypes_list)
+      continue
     if re.search("//.*Repeat for each data type", l):
       repeat = True
-      next
+      continue
     if re.search("//.*End repeat", l):
       repeat = False
       # paste the lines
-      for d in datatypes_supported:
+      for d in datatypes_list:
         outData.append( replaceFunc("\n".join(repeatData), d) )
-      next
+      repeatData = []
+      continue
+    m = re.search("CREATE_INITIALIZER[(](.*)[)]", l)
+    if(m):
+      init_name = m.group(1)
+      all_supported.extend(datatypes_list)      
+      datatypes_functions = createFunctionList(all_supported)
+
+      datatypes_list = []
+      for d in datatypes_functions:
+        if (d != "NULL"):
+          datatypes_list.append(init_name + "_" + d)
+        else:
+          datatypes_list.append(d)
+      datatype_list = ",\n      ".join( datatypes_list )
+      outData.append(datatype_list)
+      continue
     if not repeat:
       outData.append(l)
     else:
