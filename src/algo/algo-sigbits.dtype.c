bb18cfac7bea6000b73cb8128a9e726aa12f4ca8
Anastasiia Novikova <anastasiia.novikova@gmx.de>
2017-07-11 15:47:14 +0200

    sigbits supports fill_value

--- src/algo/algo-sigbits.dtype.c
+++ src/algo/algo-sigbits.dtype.c
@@ -24,7 +24,7 @@
 
 #include <math.h>
 
-#define SCIL_SIGBITS_HEADER_SIZE 5
+#define SCIL_SIGBITS_HEADER_SIZE 13
 
 static uint64_t mask[] = {
     0,
@@ -87,7 +87,9 @@ static void read_header(const byte* source,
                         uint8_t* signs_id,
                         uint8_t* exponent_bit_count,
                         uint8_t* mantissa_bit_count,
-                        int16_t* minimum_exponent){
+                        int16_t* minimum_exponent,
+                        double *fill_value,
+                        uint8_t *fill_value_exponent){
 
     *signs_id = *((uint8_t*)source);
     source += 1;
@@ -104,13 +106,25 @@ static void read_header(const byte* source,
     *minimum_exponent = *((int16_t*)source);
     source += 2;
     *source_size -= 2;
+
+    scilU_unpack8(source, fill_value);
+    source += 8;
+    *source_size -= 8;
+
+    if(*fill_value != DBL_MAX){
+      *fill_value_exponent = *((int8_t*)source);
+      source += 1;
+      *source_size -= 1;
+    }
 }
 
 static void write_header(byte* dest,
                          uint8_t signs_id,
                          uint8_t exponent_bit_count,
                          uint8_t mantissa_bit_count,
-                         uint16_t minimum_exponent){
+                         uint16_t minimum_exponent,
+                         double fill_value,
+                         uint8_t fill_value_exponent){
 
     *dest = signs_id;
     ++dest;
@@ -123,6 +137,14 @@ static void write_header(byte* dest,
 
     *((int16_t*)dest) = minimum_exponent;
     dest += 2;
+
+    scilU_pack8(dest, fill_value);
+    dest += 8;
+
+    if (fill_value != DBL_MAX){
+      *dest = fill_value_exponent;
+      ++dest;
+    }
 }
 
 static uint8_t calc_sign_bit_count(uint8_t minimum_sign, uint8_t maximum_sign){
@@ -198,6 +220,36 @@ static void find_minimums_and_maximums_<DATATYPE>(const <DATATYPE>* buffer,
     }
 }
 
+static void find_minimums_and_maximums_fill_<DATATYPE>(const <DATATYPE>* buffer,
+                                                  const size_t size,
+                                                  uint8_t* minimum_sign,
+                                                  uint8_t* maximum_sign,
+                                                  int16_t* minimum_exponent,
+                                                  int16_t* maximum_exponent,
+                                                  double fill_value){
+
+    *minimum_sign = 1;
+    *maximum_sign = 0;
+
+    *minimum_exponent = 0x7fff;
+    *maximum_exponent = -*minimum_exponent;
+
+    for(size_t i = 0; i < size; ++i){
+
+        if (buffer[i] != fill_value)
+        {
+        datatype_cast_<DATATYPE> cur;
+        cur.f = buffer[i];
+
+        if(*minimum_sign != 0 && cur.p.sign < *minimum_sign) { *minimum_sign = cur.p.sign; }
+        if(*maximum_sign != 1 && cur.p.sign > *maximum_sign) { *maximum_sign = cur.p.sign; }
+
+        if(cur.p.exponent < *minimum_exponent) { *minimum_exponent = cur.p.exponent; }
+        if(cur.p.exponent > *maximum_exponent) { *maximum_exponent = cur.p.exponent; }
+      }
+    }
+}
+
 static void get_header_data_<DATATYPE>(const <DATATYPE>* source,
                                        size_t count,
                                        uint8_t* signs_id,
@@ -212,6 +264,27 @@ static void get_header_data_<DATATYPE>(const <DATATYPE>* source,
                                           &maximum_sign,
                                           minimum_exponent,
                                           &maximum_exponent);
+
+    *signs_id = calc_sign_bit_count(minimum_sign, maximum_sign);
+    *exponent_bit_count = calc_exponent_bit_count(*minimum_exponent, maximum_exponent);
+}
+
+static void get_header_data_fill_<DATATYPE>(const <DATATYPE>* source,
+                                       size_t count,
+                                       uint8_t* signs_id,
+                                       uint8_t* exponent_bit_count,
+                                       int16_t* minimum_exponent,
+                                       double fill_value){
+
+    uint8_t minimum_sign, maximum_sign;
+    int16_t maximum_exponent;
+    find_minimums_and_maximums_fill_<DATATYPE>(source,
+                                          count,
+                                          &minimum_sign,
+                                          &maximum_sign,
+                                          minimum_exponent,
+                                          &maximum_exponent,
+                                          fill_value);
 
     *signs_id = calc_sign_bit_count(minimum_sign, maximum_sign);
     *exponent_bit_count = calc_exponent_bit_count(*minimum_exponent, maximum_exponent);
@@ -296,6 +369,31 @@ static int compress_buffer_<DATATYPE>(uint64_t* restrict dest,
     return SCIL_NO_ERR;
 }
 
+static int compress_buffer_fill_<DATATYPE>(uint64_t* restrict dest,
+                                      const <DATATYPE>* restrict source,
+                                      size_t count,
+                                      uint8_t signs_id,
+                                      uint8_t exponent_bit_count,
+                                      uint8_t mantissa_bit_count,
+                                      int16_t minimum_exponent,
+                                      double fill_value,
+                                      uint8_t fill_value_exponent){
+
+    for(size_t i = 0; i < count; ++i){
+      if (source[i] != fill_value){
+        dest[i] = compress_value_<DATATYPE>(source[i], signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent);
+      }else{
+        uint64_t result = 0;
+
+        result |= fill_value_exponent;
+        result <<= mantissa_bit_count;
+        dest[i] = result;
+      }
+    }
+
+    return SCIL_NO_ERR;
+}
+
 static int decompress_buffer_<DATATYPE>(<DATATYPE>* restrict dest,
                                         const uint64_t* restrict source,
                                         size_t count,
@@ -307,6 +405,30 @@ static int decompress_buffer_<DATATYPE>(<DATATYPE>* restrict dest,
 
     for (size_t i = 0; i < count; ++i) {
         dest[i] = decompress_value_<DATATYPE>(source[i], bit_count_per_value, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent);
+    }
+
+    return SCIL_NO_ERR;
+}
+
+static int decompress_buffer_fill_<DATATYPE>(<DATATYPE>* restrict dest,
+                                        const uint64_t* restrict source,
+                                        size_t count,
+                                        uint8_t bit_count_per_value,
+                                        uint8_t signs_id,
+                                        uint8_t exponent_bit_count,
+                                        uint8_t mantissa_bit_count,
+                                        int16_t minimum_exponent,
+                                        double fill_value,
+                                        uint8_t fill_value_exponent){
+
+    uint64_t fill_mask = fill_value_exponent << (mantissa_bit_count-1);
+
+    for(size_t i = 0; i < count; ++i){
+      if (source[i] != fill_mask){
+        dest[i] = decompress_value_<DATATYPE>(source[i], bit_count_per_value, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent);
+      }else{
+        dest[i] = fill_value;
+      }
     }
 
     return SCIL_NO_ERR;
@@ -336,13 +458,18 @@ int scil_sigbits_compress_<DATATYPE>(const scil_context_t* ctx,
 
     uint8_t signs_id, exponent_bit_count;
     int16_t minimum_exponent;
-    get_header_data_<DATATYPE>(source, count, &signs_id, &exponent_bit_count, &minimum_exponent);
+
+    if (ctx->hints.fill_value == DBL_MAX){
+      get_header_data_<DATATYPE>(source, count, &signs_id, &exponent_bit_count, &minimum_exponent);
+    }else{ // use the fill value
+      get_header_data_fill_<DATATYPE>(source, count, &signs_id, &exponent_bit_count, &minimum_exponent, ctx->hints.fill_value);
+    }
 
     uint8_t bit_count_per_value = get_bit_count_per_value(signs_id, exponent_bit_count, mantissa_bit_count);
 
-    //printf("DEBUG %d %d %d\n", bit_count_per_value, exponent_bit_count, signs_id);
+    uint8_t fill_value_exponent = 127;
 
-    write_header(dest, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent);
+    write_header(dest, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent, ctx->hints.fill_value, fill_value_exponent);
     dest += SCIL_SIGBITS_HEADER_SIZE;
 
     *dest_size = round_up_byte(bit_count_per_value * count) + SCIL_SIGBITS_HEADER_SIZE;
@@ -354,10 +481,17 @@ int scil_sigbits_compress_<DATATYPE>(const scil_context_t* ctx,
     // Allocate intermediate buffer
     uint64_t* compressed_buffer = (uint64_t*)SAFE_MALLOC(count * sizeof(uint64_t));
 
-    // Compress each value in source buffer
-    if(compress_buffer_<DATATYPE>(compressed_buffer, source, count, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent)){
+    if (ctx->hints.fill_value == DBL_MAX){
+      // Compress each value in source buffer
+      if(compress_buffer_<DATATYPE>(compressed_buffer, source, count, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent)){
         ret = SCIL_BUFFER_ERR;
         goto comp_cleanup;
+      }
+    }else{ // don't compress the fill value
+      if(compress_buffer_fill_<DATATYPE>(compressed_buffer, source, count, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent, ctx->hints.fill_value, fill_value_exponent)){
+        ret = SCIL_BUFFER_ERR;
+        goto comp_cleanup;
+      }
     }
 
     // Pack compressed values tightly
@@ -382,6 +516,8 @@ int scil_sigbits_decompress_<DATATYPE>(<DATATYPE>*restrict dest,
     assert(dims != NULL);
     assert(source != NULL);
 
+    double fill_value = 0;
+
     // ==================== Initialization =====================================
 
     size_t count = scilPr_get_dims_count(dims);
@@ -390,7 +526,8 @@ int scil_sigbits_decompress_<DATATYPE>(<DATATYPE>*restrict dest,
 
     uint8_t signs_id, exponent_bit_count, mantissa_bit_count;
     int16_t minimum_exponent;
-    read_header(source, &source_size_cp, &signs_id, &exponent_bit_count, &mantissa_bit_count, &minimum_exponent);
+    uint8_t fill_value_exponent;
+    read_header(source, &source_size_cp, &signs_id, &exponent_bit_count, &mantissa_bit_count, &minimum_exponent, &fill_value, &fill_value_exponent);
     source += SCIL_SIGBITS_HEADER_SIZE;
 
     uint8_t bit_count_per_value = get_bit_count_per_value(signs_id, exponent_bit_count, mantissa_bit_count);
@@ -406,16 +543,17 @@ int scil_sigbits_decompress_<DATATYPE>(<DATATYPE>*restrict dest,
         goto decomp_cleanup;
     }
 
-    if(decompress_buffer_<DATATYPE>(dest,
-                                    unswaged_buffer,
-                                    count,
-                                    bit_count_per_value,
-                                    signs_id,
-                                    exponent_bit_count,
-                                    mantissa_bit_count,
-                                    minimum_exponent) ){
-        ret = SCIL_BUFFER_ERR;
-        goto decomp_cleanup;
+    if (fill_value == DBL_MAX){
+      // Deompress each value in source buffer
+      if(decompress_buffer_<DATATYPE>(dest, unswaged_buffer, count, bit_count_per_value, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent) ){
+          ret = SCIL_BUFFER_ERR;
+          goto decomp_cleanup;
+      }
+    }else{ // set fill value
+      if(decompress_buffer_fill_<DATATYPE>(dest, unswaged_buffer, count, bit_count_per_value, signs_id, exponent_bit_count, mantissa_bit_count, minimum_exponent, fill_value, fill_value_exponent) ){
+          ret = SCIL_BUFFER_ERR;
+          goto decomp_cleanup;
+      }
     }
 
     // ==================== Cleanup ============================================
