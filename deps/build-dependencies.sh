fd71854f14424acfc00b5df781cc606e6393c048
Julian M. Kunkel <juliankunkel@googlemail.com>
2017-07-29 13:50:04 +0200

    Started support for whitespace in directory name (not working due to dependencies in some src codes used).

--- deps/build-dependencies.sh
+++ deps/build-dependencies.sh
@@ -9,20 +9,20 @@ if [[ "$1" == "download" ]] ; then
 DOWNLOAD_ONLY=1
 fi
 
-mkdir -p $TGT
-cd $TGT
+mkdir -p "$TGT"
+cd "$TGT"
 echo "Building dependencies for SCIL from third-party software"
 
 function download(){
-		if [[ ! -e $SRC/$1 ]] ; then
-			wget $2/$1 -O $SRC/$1
+		if [[ ! -e "$SRC/$1" ]] ; then
+			wget "$2/$1" -O "$SRC/$1"
 		fi
 		if [[ $DOWNLOAD_ONLY == 1 ]] ; then
 			return
 		fi
 
-		if [[ ! -e $TGT/$1 ]] ; then
-			tar -xf $SRC/$1
+		if [[ ! -e "$TGT/$1" ]] ; then
+			tar -xf "$SRC/$1"
 		fi
 }
 
@@ -33,44 +33,44 @@ WAVELET=wavelet_code
 download $FPZIP.tar.gz http://computation.llnl.gov/projects/floating-point-compression/download
 download $ZFP.tar.gz http://computation.llnl.gov/projects/floating-point-compression/download
 
-if [[ ! -e $SRC/$WAVELET.zip ]] ; then
-	wget http://eeweb.poly.edu/~onur/$WAVELET.zip -O $SRC/$WAVELET.zip
+if [[ ! -e "$SRC/$WAVELET.zip" ]] ; then
+	wget http://eeweb.poly.edu/~onur/$WAVELET.zip -O "$SRC/$WAVELET.zip"
 fi
 
-if [[ ! -e $TGT/$WAVELET && $DOWNLOAD_ONLY == 0 ]] ; then
-	unzip $SRC/$WAVELET.zip -d $TGT/$WAVELET
+if [[ ! -e "$TGT/$WAVELET"&& $DOWNLOAD_ONLY == 0 ]] ; then
+	unzip "$SRC/$WAVELET.zip" -d "$TGT/$WAVELET"
 	if [[ $? != 0 ]] ; then
 		echo "Error unzip $SRC/$1"
 		exit 1
 	fi
 fi
 
-if [[ ! -e $SRC/cnoise/test/test_output.txt ]] ; then
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/cnoise.c -P $SRC/cnoise/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/cnoise.h -P $SRC/cnoise/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/README.txt -P $SRC/cnoise/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/Config.mk -P $SRC/cnoise/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/Makefile -P $SRC/cnoise/
+if [[ ! -e "$SRC/cnoise/test/test_output.txt" ]] ; then
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/cnoise.c -P "$SRC/cnoise/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/cnoise.h -P "$SRC/cnoise/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/README.txt -P "$SRC/cnoise/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/Config.mk -P "$SRC/cnoise/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/Makefile -P "$SRC/cnoise/"
 
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/test.c -P $SRC/cnoise/test/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/Makefile -P $SRC/cnoise/test/
-	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/test_output.txt -P $SRC/cnoise/test/
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/test.c -P "$SRC/cnoise/test/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/Makefile -P "$SRC/cnoise/test/"
+	wget https://people.sc.fsu.edu/~jburkardt/c_src/cnoise/example/test_output.txt -P "$SRC/cnoise/test/"
 fi
 
-if [[ ! -e $SRC/SZ ]] ; then
-	pushd $SRC
+if [[ ! -e "$SRC/SZ" ]] ; then
+	pushd "$SRC"
 	git clone https://github.com/disheng222/SZ.git
 	popd
 fi
 
-if [[ ! -e $SRC/CubismZ ]] ; then
-	pushd $SRC
+if [[ ! -e "$SRC/CubismZ" ]] ; then
+	pushd "$SRC"
 	git clone https://github.com/cselab/CubismZ.git
 	popd
 fi
 
-if [[ ! -e $SRC/lz4 ]] ; then
-	pushd $SRC
+if [[ ! -e "$SRC/lz4" ]] ; then
+	pushd "$SRC"
 	git clone https://github.com/lz4/lz4.git
 	popd
 fi
@@ -80,8 +80,8 @@ if [[ $DOWNLOAD_ONLY == 1 ]] ; then
 fi
 
 
-if [[ ! -e $TGT/cnoise/ ]] ; then
-	cp -r $SRC/cnoise/ .
+if [[ ! -e "$TGT/cnoise/" ]] ; then
+	cp -r "$SRC/cnoise/" .
 fi
 
 
@@ -89,8 +89,8 @@ BUILD=0
 
 if [[ ! -e libzfp.a ]] ; then
 	echo "  Building fpzip shared library"
-	pushd $ZFP > /dev/null
-	cp $SRC/config-zfp Config
+	pushd "$ZFP" > /dev/null
+	cp "$SRC/config-zfp" Config
 	make shared
 	make
 	popd > /dev/null
@@ -99,8 +99,8 @@ fi
 
 if [[ ! -e libfpzip.a ]] ; then
   echo "  Building zfp shared library"
-  pushd $FPZIP/src > /dev/null
-  make -f $SRC/Makefile-fpzip-1.1.0
+  pushd "$FPZIP/src" > /dev/null
+  make -f "$SRC/Makefile-fpzip-1.1.0"
   popd > /dev/null
 	BUILD=1
 fi
@@ -117,7 +117,7 @@ fi
 
 if [[ ! -e liblz4.a ]] ; then
 	mkdir -p include/lz4 || true
-	pushd $SRC/lz4/ > /dev/null
+	pushd "$SRC/lz4/" > /dev/null
 	make clean || true
 	make -j 4 CFLAGS="-fPIC"
 	popd > /dev/null
@@ -128,7 +128,7 @@ if [[ ! -e libsz.a ]] ; then
 	echo "  Building SZ"
 	mkdir SZ || true
 	pushd SZ > /dev/null
-	CFLAGS="-I$SRC/SZ/sz/include -I$SRC/SZ/zlib -fPIC -O3"  $SRC/SZ/configure  --disable-shared --prefix=$PWD/install
+	CFLAGS="-I\"$SRC/SZ/sz/include\" -I\"$SRC/SZ/zlib\" -fPIC -O3"  "$SRC/SZ/configure"  --disable-shared --prefix="$PWD/install"
 	make -j install
 	popd > /dev/null
 	BUILD=1
