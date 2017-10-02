f7dae4bb294aca6e56cdae8ffaefb2b4605ffacf
Julian Kunkel <juliankunkel@googlemail.com>
2017-10-02 11:00:46 +0200

    Towards Fortran.

--- tools/hdf5-plugin/test/fortran/interface-test.c
+++ tools/hdf5-plugin/test/fortran/interface-test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <scil.h>
+#include <scil-hdf5-plugin.h>
 
-void h5pset_scil_compression_hints_f(int a, int *b, double c)
+void h5pset_scil_compression_hints_f(hid_t dcpl, int a, int *b, double c)
 {
+  scil_user_hints_t hints;
+  scilPr_initialize_user_hints(& hints);
+  //H5Pset_scil_user_hints_t(hid_t dcpl, scil_user_hints_t * hints);
+
   printf("%s%d %f\n", "Ergebnis = ", a * *b, c);
 }
