b6635010d68313563c53b277042f18e606d3ca16
Anastasiia Novikova <anastasiia.novikova@gmx.de>
2017-10-02 14:10:54 +0200

    prepare fix

--- tools/hdf5-plugin/test/fortran/prepare.sh
+++ tools/hdf5-plugin/test/fortran/prepare.sh
@@ -3,7 +3,7 @@ if [[ ! -e hdf5-1.10.1.tar.gz ]] ; then
   wget https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.10.1.tar.gz  
 fi
 
-tar -cf hdf5-1.10.1.tar.gz
+tar -xf hdf5-1.10.1.tar.gz
 
 PREFIX=$PWD/install
 
@@ -15,7 +15,6 @@ pushd hdf5-1.10.1
 	--enable-parallel \
 	--enable-build-mode=debug \
 	--enable-fortran \
-	--enable-fortran2003 \
 	CC="mpicc" \
 	FC="mpif90" \
 	F77="mpi77" \
