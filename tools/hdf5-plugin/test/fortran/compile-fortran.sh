836d617a8128633c9a95f361292bfad4e6d6ef72
Anastasiia Novikova <anastasiia.novikova@gmx.de>
2017-10-04 17:41:17 +0200

    toward hdf fortran fix

--- tools/hdf5-plugin/test/fortran/compile-fortran.sh
+++ tools/hdf5-plugin/test/fortran/compile-fortran.sh
@@ -7,4 +7,5 @@ gfortran -c scil-mod.f95
 
 mpicc -c interface-test.c -I$SCIL/include 
 gfortran -c -fno-underscoring interface.f95 -I$install/include -I$SCIL/include
-gfortran interface.o interface-test.o -o interface.exe -L$install/lib/ -lhdf5_fortran -lhdf5 -Wl,--rpath=$install/lib
+
+gfortran interface.o interface-test.o -o interface.exe -L$SCIL/lib -L$install/lib/ -lhdf5_fortran -lhdf5 -lscil -lhdf5-filter-scil -Wl,--rpath=$install/lib
--- tools/hdf5-plugin/test/fortran/interface-test.c
+++ tools/hdf5-plugin/test/fortran/interface-test.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <scil.h>
-#include <scil-util.h>
 #include <assert.h>
 #include <hdf5.h>
 #include <string.h>
+
 #include <scil-hdf5-plugin.h>
+
+#include <scil.h>
+#include <scil-util.h>
 
 void h5pset_scil_compression_hints_f(hid_t dcpl, int a, int *b, double c)
 {
   scil_user_hints_t hints;
-  //scilPr_initialize_user_hints(& hints);
-  //H5Pset_scil_user_hints_t(hid_t dcpl, scil_user_hints_t * hints);
+  scilPr_initialize_user_hints(& hints);
+  H5Pset_scil_user_hints_t(dcpl, & hints);
 
   printf("%s%d %f\n", "Ergebnis = ", a * *b, c);
 }
