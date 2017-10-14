bdb7843f3b8100e55a795e03d4c64ac827695576
Julian M. Kunkel <juliankunkel@googlemail.com>
2017-10-14 22:03:17 +0200

    Proper cleanup dynamic symbol table -- and helper script to update exported symbols.

--- dev/symbols-create.sh
+++ dev/symbols-create.sh
@@ -1,7 +1,37 @@
-#!/bin/bash
+#!/bin/bash -e
+
+echo "Rebuild after setting:"
+cat << EOF > src/symbols.map
+{
+ global:*;
+};
+EOF
+
+echo "Now make clean, rebuild, then press enter!"
+pushd build
+make clean
+make -j
+popd
 
 (
-nm build/util/libscil-util.so
-nm build/compression/libscil.so
-nm build/pattern/libscil-patterns.so
-) |cut -b "20-" |grep scil > src/symbols.txt
+for F in $(find build -name "*.so" |grep -v deps) ; do
+nm --defined-only $F
+done
+) |cut -b "20-" | grep scil > src/symbols.txt
+
+(
+echo "{"
+echo " global:"
+for s in $(cat src/symbols.txt) ; do
+  echo "$s;"
+done
+echo "  local:*;"
+echo "};"
+) > src/symbols.map
+
+
+echo "Complete! Now make clean and rebuild"
+pushd build
+make clean
+make -j
+popd
